e121b80f185048cd849a5969b819e27822c546b3
Witold <blue3656@gmail.com>
2017-01-19 22:19:36 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(json){
+
+		
+
+	});
+
+
+
+});
