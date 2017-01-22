5e4697bf7dc2b0cd305cc66661b8c038e368100d
Witold <blue3656@gmail.com>
2017-01-22 00:07:25 +0000

    updated js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 
-	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(json){
+	$("#searchBtn").on("click", function(){
+		var searchTerm = $("#searchLine").val();
+		var wikiLink = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
 
 		
+		$.ajax({
+		    url: wikiLink,
+		    type: 'GET',
+		    dataType: "json",
+		    contentType: "application/json; charset=utf-8",
+		    headers: { 'Api-User-Agent': 'Witold_wikiApp/1.0' },
+		    async: false,
+		    success: function(data, status, jqXHR) {
+		       // do something with data
+		       //$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(data){
+		       		console.log(data);
+		      // });
+		    }
+		});
 
 	});
-
-
 
 });
