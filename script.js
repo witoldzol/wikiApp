94f7554a63a1a60ac2375b0e631cd8d9a77c2598
Witold <blue3656@gmail.com>
2017-01-22 20:35:54 +0000

    finished main functionalities

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
 $(document).ready(function(){
+	//opens random page
+	$("#random").on("click", function(){
+		var randomWiki = "https://en.wikipedia.org/wiki/Special:Random";
+		window.open(randomWiki, '_blank');
+	});	 
+
+	$("#searchLine").on("keyup", function(){
+		//our input in search bar
+		var searchTerm = $("#searchLine").val();
+		//link to wiki api
+		var wikiLink = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+		$.getJSON(wikiLink, function(data){
+			//console.log(data[1]);
+			//$(function() {
+				var list = data[1];
+				$("#searchLine").autocomplete({
+					source: list
+				});
+				
+			//});
+		});
+	});	
 
 	$("#searchBtn").on("click", function(){
-		var searchTerm = $("#searchLine").val();
-		var wikiLink = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
 
-		
+		//our input in search bar
+		var searchTerm = $("#searchLine").val();
+		//link to wiki api
+		if(searchTerm == ''){
+			
+		}else
+		var wikiLink = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+		//clears all elements from #results elements so if we make a new search, old results will get deleted
+		$("#results").empty();
 		$.ajax({
 		    url: wikiLink,
 		    type: 'GET',
+		    	cache: false,
 		    dataType: "json",
-		    contentType: "application/json; charset=utf-8",
 		    headers: { 'Api-User-Agent': 'Witold_wikiApp/1.0' },
 		    async: false,
-		    success: function(data, status, jqXHR) {
-		       // do something with data
-		       //$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", function(data){
-		       		console.log(data);
-		      // });
-		    }
+		    contentType: "application/json",
+		    	success: function(data) {
+		    		console.log(data);
+		    		//we loop through results, get headers and descriptions and create p elements to store them in
+		    		if (data[1].length == 0){
+		    			$("#results").html('No results found, try different query');
+		    		}else
+			    		for (i=0; i<data[1].length; i++){
+
+				    		$("#results").append($("<a target='blank' href="+data[3][i]+ "> <p id=\"pageLink\"><strong>"+data[1][i]+"</strong><br>"+data[2][i]+"</p></a>"));
+			    			
+			    		}
+		    	
+ 		    	},
+ 		    	error: function(errorMsg){
+ 		    	alert('Search box is empty, please put something in.');
+ 		    	}
 		});
+
+	
+		 
+
 
 	});
 
